527edb9f2409bc5cf072e309dde044ef6bcffabd
Markus Seven <markus.seven@outlook.com>
2025-03-27 12:55:25 +0100

    initial commit

--- src/main/java/com/example/lesson/LessonApplication.java
+++ src/main/java/com/example/lesson/LessonApplication.java
@@ -2,11 +2,7 @@ package com.example.lesson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
-
-import java.io.Console;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class LessonApplication {
